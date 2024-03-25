fdb7ec9bc496999b20ff8651cc24c035e72893ce
xunaiet-faruk <xunaiet28@gmail.com>
2024-03-26 00:41:33 +0600

    modal form

--- src/TaskBoard/BoardButton.jsx
+++ src/TaskBoard/BoardButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import AddtaskModal from "./TaskWork/AddtaskModal";
-import { TiDeleteOutline } from "react-icons/ti";
+
 
 
 const BoardButton = () => {
@@ -54,16 +54,15 @@ const BoardButton = () => {
             {/* here the modal  */}
             <div className="">
                 <div onClick={() => setOpenModal(false)} className={`fixed z-[100] flex items-center justify-center ${openModal ? 'visible opacity-100 ' : 'invisible opacity-0 '} inset-0 bg-black/20 backdrop-blur-sm  duration-100 dark:bg-white/10`}>
-                    <div onClick={(e_) => e_.stopPropagation()} className={`text- absolute max-w-md w-full md:max-w-2xl rounded-sm bg-gradient-to-r from-[#f4dbf9] via-[#e9dbfc] to-[#e0dafc] py-6 drop-shadow-lg dark:bg-black dark:text-white ${openModal ? 'scale-1 opacity-1 duration-300' : 'scale-0 opacity-0 duration-150'}`}>
-                        <div className="flex justify-end ">
-                            <button onClick={() => setOpenModal(false)} className="rounded-sm border   duration-150  hover:text-white"><TiDeleteOutline className="text-4xl hover:text-red-400" /></button>
-                        </div>
-                        <AddtaskModal />
+                    <div onClick={(e_) => e_.stopPropagation()} className={`text- absolute max-w-md w-full md:max-w-2xl rounded-sm bg-gradient-to-r from-[#f4dbf9] via-[#e9dbfc] to-[#e0dafc] drop-shadow-lg dark:bg-black dark:text-white ${openModal ? 'scale-1 opacity-1 duration-300' : 'scale-0 opacity-0 duration-150'}`}>
+                       
+                        <AddtaskModal setOpenModal={setOpenModal}/>
+                        
 
                     </div>
-                    
+
                 </div>
-                
+
             </div>
 
         </div>
--- src/TaskBoard/TaskWork/AddtaskModal.jsx
+++ src/TaskBoard/TaskWork/AddtaskModal.jsx
@@ -1,12 +1,14 @@
-const AddtaskModal = () => {
+import { TiDeleteOutline } from "react-icons/ti";
+const AddtaskModal = ({ setOpenModal }) => {
     return (
-        <div className="">
-            <h1 className="text-2xl font-bold  w-full h-full  m-0">Create A Task </h1>
+        <div className="space-y-12">
+            
 
-            <div className="border-2 border-white">
-
+            <div className="flex justify-between ">
+                <h1 className="text-2xl bg-white py-5 pl-4 font-bold  w-full h-full  m-0">Create A Task </h1>
+                <button onClick={() => setOpenModal(false)} className="bg-white pr-5"><TiDeleteOutline className="text-4xl hover:text-red-400" /></button>
             </div>
-            <div className=" bg-gradient-to-r from-[#f4dbf9] via-[#e9dbfc] to-[#e0dafc]">
+            <div className=" bg-gradient-to- space-y-10 from-[#f4dbf9] via-[#e9dbfc] to-[#e0dafc]">
                 {/* Center every line vertically */}
                 <div className="flex items-center justify-between px-7 my-5">
                     <h1 className="text-xl font-semibold m-0">Tiltle  :  </h1>
@@ -26,7 +28,7 @@ const AddtaskModal = () => {
                 </div>
                 <div className="flex  px-7 items-center gap-24 my-5">
                     <h1 className="text-xl font-semibold mt-2 m-0">Priority :  </h1>
-                    <select name="" id="" className="bg-[#dadada] py-1  px-3 border-2 border-[#acabad] rounded-md">
+                    <select name="" id="" className="bg-[#dadada] ml-12 py-1  px-3 border-2 border-[#acabad] rounded-md">
                         <option value="P0">P0</option>
                         <option value="P1">P1</option>
                         <option value="P2">P2</option>
@@ -34,8 +36,11 @@ const AddtaskModal = () => {
                 </div>
             </div>
 
-            <div className="bg-white h-full">
-                djsajdfka
+            <div className="bg-white  h-full">
+                <div className="flex justify-end items-center gap-3 p-4">
+                    <input type="submit" value="Submit" className="bg-[#25669b] text-white px-9 py-1 rounded-md " />
+                </div>
+
             </div>
 
         </div>
